94464e5f81eeb17146f46f8d00113a19cf10857e
seav1 <1@seav.eu.org>
2024-07-31 13:42:17 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -272,7 +272,7 @@ stderr_logfile=/dev/null
 stdout_logfile=/dev/null
 
 [program:agent]
-command=$WORK_DIR/nezha-agent -s localhost:$GRPC_PORT -p --disable-auto-update --disable-force-update $LOCAL_TOKEN
+command=$WORK_DIR/nezha-agent -s localhost:$GRPC_PORT --disable-auto-update --disable-force-update -p $LOCAL_TOKEN
 autostart=true
 autorestart=true
 stderr_logfile=/dev/null
